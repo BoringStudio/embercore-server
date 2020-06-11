de61b3f64e5970fb7693ebdfa2fe76b97fa26405
LazyMechanic <AsharnRus@gmail.com>
2020-06-11 11:03:40 +0300

    Add tme::Chunk

--- src/tme/models/chunk.rs
+++ src/tme/models/chunk.rs
@@ -1,4 +1,12 @@
+use super::redata::ReData;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Chunk {}
+#[serde(rename_all = "lowercase")]
+pub struct Chunk {
+    data:   ReData,
+    height: i32,
+    width:  i32,
+    x:      i32,
+    y:      i32,
+}
