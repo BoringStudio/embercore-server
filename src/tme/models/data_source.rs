0ec2475cd0d1506ee38351dc31fc6843b29e6e27
LazyMechanic <AsharnRus@gmail.com>
2020-06-15 20:34:30 +0300

    Add DataSource tests

--- src/tme/models/data_source.rs
+++ src/tme/models/data_source.rs
@@ -19,3 +19,82 @@ impl FromStr for DataSource {
         Ok(DataSource::Encoded(s.to_owned()))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::tme::models::data_source::DataSource::{Encoded, Raw};
+    use lazy_static::*;
+
+    #[derive(Debug, Deserialize, Serialize, PartialEq)]
+    struct Wrapper {
+        data: DataSource,
+    }
+
+    impl Wrapper {
+        pub fn new(data: DataSource) -> Self {
+            Self { data }
+        }
+    }
+
+    lazy_static! {
+        static ref DE_DATA_SOURCE_STR: String = r#"
+            [
+                {
+                    "data": "qweasdzxcQWEASDZXC"
+                },
+                {
+                    "data": [0, 0, 1, 0, 1]
+                }
+            ]
+        "#
+        .to_string();
+        static ref SER_DATA_SOURCE_STR: Vec<String> = vec![
+            r#"
+                {
+                    "data": "qweasdzxcQWEASDZXC"
+                }
+            "#
+            .to_string()
+            .replace(' ', "")
+            .replace('\n', ""),
+            r#"
+                {
+                    "data": [0, 0, 1, 0, 1]
+                }
+            "#
+            .to_string()
+            .replace(' ', "")
+            .replace('\n', ""),
+        ];
+    }
+
+    #[test]
+    fn deserialize_data_source() {
+        let actuals: Vec<Wrapper> = serde_json::from_str(DE_DATA_SOURCE_STR.as_str()).unwrap();
+
+        let expecteds: Vec<Wrapper> = vec![
+            Wrapper::new(Encoded("qweasdzxcQWEASDZXC".to_owned())),
+            Wrapper::new(Raw(vec![0, 0, 1, 0, 1])),
+        ];
+
+        for (actual, expected) in actuals.into_iter().zip(expecteds) {
+            assert_eq!(actual, expected);
+        }
+    }
+
+    #[test]
+    fn serialize_data_source() {
+        let expecteds: Vec<String> = SER_DATA_SOURCE_STR.to_vec();
+
+        let actuals: Vec<String> = vec![
+            serde_json::to_string(&Wrapper::new(Encoded("qweasdzxcQWEASDZXC".to_owned()))).unwrap(),
+            serde_json::to_string(&Wrapper::new(Raw(vec![0, 0, 1, 0, 1]))).unwrap(),
+        ];
+
+        for (actual, expected) in actuals.into_iter().zip(expecteds) {
+            assert_eq!(actual, expected);
+        }
+    }
+}
