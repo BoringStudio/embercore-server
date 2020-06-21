ceb0b00d62138f3d7930d5b881cc8adeb381fd2b
LazyMechanic <AsharnRus@gmail.com>
2020-06-21 19:09:07 +0300

    Add TileLayer tests

--- src/tme/models/tile_layer.rs
+++ src/tme/models/tile_layer.rs
@@ -39,98 +39,13 @@ mod tests {
 
     use crate::tme::models::data_source::DataSource::{Encoded, Raw};
     use crate::tme::models::layer::Layer;
-
-    const LAYERS_STR: &'static str = r#"
-            [
-              {
-                "data":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                "height":100,
-                "id":9,
-                "name":"L1",
-                "opacity":1,
-                "type":"tilelayer",
-                "visible":true,
-                "width":100,
-                "x":0,
-                "y":0
-              },
-              {
-                "data": "MCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMA==",
-                "encoding": "base64",
-                "height":100,
-                "id":9,
-                "name":"L1",
-                "opacity":1,
-                "type":"tilelayer",
-                "visible":true,
-                "width":100,
-                "x":0,
-                "y":0
-              },
-              {
-                "chunks": [
-                  {
-                    "data": [1, 1, 0, 1],
-                    "height": 15,
-                    "width": 55,
-                    "x": 77,
-                    "y": 88
-                  },
-                  {
-                    "data": "MSwgMSwgMCwgMQ==",
-                    "height": 6,
-                    "width": 66,
-                    "x": 0,
-                    "y": 0
-                  }
-                ],
-                "data": "MCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMA==",
-                "encoding": "base64",
-                "height":100,
-                "id":9,
-                "name":"L1",
-                "opacity":1,
-                "type":"tilelayer",
-                "visible":true,
-                "width":100,
-                "x":0,
-                "y":0
-              },
-              {
-                "compression": "zlib",
-                "data": "eNrzdS5P9yUFO9raAgDo2RIx",
-                "encoding": "base64",
-                "height":100,
-                "id":9,
-                "name":"L1",
-                "opacity":1,
-                "type":"tilelayer",
-                "visible":true,
-                "width":100,
-                "x":0,
-                "y":0
-              },
-              {
-                "compression": "gzip",
-                "data": "H4sIAAAAAAAA//N1Lk/3JQU72toCAHCxT1Y0AAAA",
-                "encoding": "base64",
-                "height":100,
-                "id":9,
-                "name":"L1",
-                "opacity":1,
-                "type":"tilelayer",
-                "visible":true,
-                "width":100,
-                "x":0,
-                "y":0
-              }
-            ]
-        "#;
+    use crate::tme::models::tile::Tile;
+    use serde_json::json;
 
     #[test]
     fn deserialize_tile_layer() {
         let expected_layers = vec![
-            Layer::TileLayer(TileLayer {
+            TileLayer {
                 chunks:      None,
                 compression: None,
                 data:        Raw(vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
@@ -148,8 +63,8 @@ mod tests {
                 width:       100,
                 x:           0,
                 y:           0,
-            }),
-            Layer::TileLayer(TileLayer {
+            },
+            TileLayer {
                 chunks:      None,
                 compression: None,
                 data:        Encoded(
@@ -169,8 +84,8 @@ mod tests {
                 width:       100,
                 x:           0,
                 y:           0,
-            }),
-            Layer::TileLayer(TileLayer {
+            },
+            TileLayer {
                 chunks:      Some(vec![
                     Chunk {
                         data:   Raw(vec![1, 1, 0, 1]),
@@ -205,8 +120,8 @@ mod tests {
                 width:       100,
                 x:           0,
                 y:           0,
-            }),
-            Layer::TileLayer(TileLayer {
+            },
+            TileLayer {
                 chunks:      None,
                 compression: Some(Compression::Zlib),
                 data:        Encoded("eNrzdS5P9yUFO9raAgDo2RIx".to_owned()),
@@ -224,8 +139,8 @@ mod tests {
                 width:       100,
                 x:           0,
                 y:           0,
-            }),
-            Layer::TileLayer(TileLayer {
+            },
+            TileLayer {
                 chunks:      None,
                 compression: Some(Compression::Gzip),
                 data:        Encoded("H4sIAAAAAAAA//N1Lk/3JQU72toCAHCxT1Y0AAAA".to_owned()),
@@ -243,22 +158,377 @@ mod tests {
                 width:       100,
                 x:           0,
                 y:           0,
-            }),
+            },
         ];
 
-        let actual_layers: Vec<Layer> = serde_json::from_str(LAYERS_STR).unwrap();
+        let actual_layers: Vec<TileLayer> = serde_json::from_value(json! {
+            [
+                {
+                    "chunks": null,
+                    "compression": null,
+                    "data":[0,0,0,0,0,0,0,0,0,0,0,0,0],
+                    "encoding": null,
+                    "height":100,
+                    "id":9,
+                    "name":"L1",
+                    "offsetx": null,
+                    "offsety": null,
+                    "opacity":1.0,
+                    "startx": null,
+                    "starty": null,
+                    "visible":true,
+                    "width":100,
+                    "x":0,
+                    "y":0
+                },
+                {
+                    "chunks": null,
+                    "compression": null,
+                    "data": "MCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMA==",
+                    "encoding": "base64",
+                    "height":100,
+                    "id":9,
+                    "name":"L1",
+                    "offsetx": null,
+                    "offsety": null,
+                    "opacity":1.0,
+                    "startx": null,
+                    "starty": null,
+                    "visible":true,
+                    "width":100,
+                    "x":0,
+                    "y":0
+                },
+                {
+                    "chunks": [
+                        {
+                            "data": [1, 1, 0, 1],
+                            "height": 15,
+                            "width": 55,
+                            "x": 77,
+                            "y": 88
+                        },
+                        {
+                            "data": "MSwgMSwgMCwgMQ==",
+                            "height": 6,
+                            "width": 66,
+                            "x": 0,
+                            "y": 0
+                        }
+                    ],
+                    "compression": null,
+                    "data": "MCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMA==",
+                    "encoding": "base64",
+                    "height":100,
+                    "id":9,
+                    "name":"L1",
+                    "offsetx": null,
+                    "offsety": null,
+                    "opacity":1.0,
+                    "startx": null,
+                    "starty": null,
+                    "visible":true,
+                    "width":100,
+                    "x":0,
+                    "y":0
+                },
+                {
+                    "chunks": null,
+                    "compression": "zlib",
+                    "data": "eNrzdS5P9yUFO9raAgDo2RIx",
+                    "encoding": "base64",
+                    "height":100,
+                    "id":9,
+                    "name":"L1",
+                    "opacity":1.0,
+                    "startx": null,
+                    "starty": null,
+                    "visible":true,
+                    "width":100,
+                    "x":0,
+                    "y":0
+                },
+                {
+                    "chunks": null,
+                    "compression": "gzip",
+                    "data": "H4sIAAAAAAAA//N1Lk/3JQU72toCAHCxT1Y0AAAA",
+                    "encoding": "base64",
+                    "height":100,
+                    "id":9,
+                    "name":"L1",
+                    "offsetx": null,
+                    "offsety": null,
+                    "opacity":1.0,
+                    "startx": null,
+                    "starty": null,
+                    "visible":true,
+                    "width":100,
+                    "x":0,
+                    "y":0
+                }
+            ]
+        })
+        .unwrap();
 
         for (actual, expected) in actual_layers.into_iter().zip(expected_layers) {
-            match actual {
-                Layer::TileLayer(a) => {
-                    if let Layer::TileLayer(e) = expected {
-                        assert_eq!(a, e);
-                    } else {
-                        panic!("invalid expected layer type")
-                    }
+            assert_eq!(actual, expected);
+        }
+    }
+
+    #[test]
+    fn serialize_tile_layer() {
+        let expecteds: Vec<String> = vec![
+            serde_json::to_string(&json! {
+                {
+                    "chunks": null,
+                    "compression": null,
+                    "data":[0,0,0,0,0,0,0,0,0,0,0,0,0],
+                    "encoding": null,
+                    "height":100,
+                    "id":9,
+                    "name":"L1",
+                    "offsetx": null,
+                    "offsety": null,
+                    "opacity":1.0,
+                    "properties": null,
+                    "startx": null,
+                    "starty": null,
+                    "visible":true,
+                    "width":100,
+                    "x":0,
+                    "y":0
                 }
-                _ => panic!("invalid actual layer type"),
-            }
+            })
+            .unwrap(),
+            serde_json::to_string(&json! {
+                {
+                    "chunks": null,
+                    "compression": null,
+                    "data": "MCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMA==",
+                    "encoding": "base64",
+                    "height":100,
+                    "id":9,
+                    "name":"L1",
+                    "offsetx": null,
+                    "offsety": null,
+                    "opacity":1.0,
+                    "properties": null,
+                    "startx": null,
+                    "starty": null,
+                    "visible":true,
+                    "width":100,
+                    "x":0,
+                    "y":0
+                }
+            })
+            .unwrap(),
+            serde_json::to_string(&json! {
+                {
+                    "chunks": [
+                    {
+                        "data": [1, 1, 0, 1],
+                        "height": 15,
+                        "width": 55,
+                        "x": 77,
+                        "y": 88
+                    },
+                    {
+                        "data": "MSwgMSwgMCwgMQ==",
+                        "height": 6,
+                        "width": 66,
+                        "x": 0,
+                        "y": 0
+                    }
+                    ],
+                    "compression": null,
+                    "data": "MCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMA==",
+                    "encoding": "base64",
+                    "height":100,
+                    "id":9,
+                    "name":"L1",
+                    "offsetx": null,
+                    "offsety": null,
+                    "opacity":1.0,
+                    "properties": null,
+                    "startx": null,
+                    "starty": null,
+                    "visible":true,
+                    "width":100,
+                    "x":0,
+                    "y":0
+                }
+            })
+            .unwrap(),
+            serde_json::to_string(&json! {
+                {
+                    "chunks": null,
+                    "compression": "zlib",
+                    "data": "eNrzdS5P9yUFO9raAgDo2RIx",
+                    "encoding": "base64",
+                    "height":100,
+                    "id":9,
+                    "name":"L1",
+                    "offsetx": null,
+                    "offsety": null,
+                    "opacity":1.0,
+                    "properties": null,
+                    "startx": null,
+                    "starty": null,
+                    "visible":true,
+                    "width":100,
+                    "x":0,
+                    "y":0
+                }
+            })
+            .unwrap(),
+            serde_json::to_string(&json! {
+                {
+                    "chunks": null,
+                    "compression": "gzip",
+                    "data": "H4sIAAAAAAAA//N1Lk/3JQU72toCAHCxT1Y0AAAA",
+                    "encoding": "base64",
+                    "height":100,
+                    "id":9,
+                    "name":"L1",
+                    "offsetx": null,
+                    "offsety": null,
+                    "opacity":1.0,
+                    "properties": null,
+                    "startx": null,
+                    "starty": null,
+                    "visible":true,
+                    "width":100,
+                    "x":0,
+                    "y":0
+                }
+            })
+            .unwrap(),
+        ];
+
+        let actuals: Vec<String> = vec![
+            serde_json::to_string(&TileLayer {
+                chunks:      None,
+                compression: None,
+                data:        Raw(vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+                encoding:    None,
+                height:      100,
+                id:          9,
+                name:        "L1".to_owned(),
+                offset_x:    None,
+                offset_y:    None,
+                opacity:     1.0,
+                properties:  None,
+                start_x:     None,
+                start_y:     None,
+                visible:     true,
+                width:       100,
+                x:           0,
+                y:           0,
+            })
+            .unwrap(),
+            serde_json::to_string(&TileLayer {
+                chunks:      None,
+                compression: None,
+                data:        Encoded(
+                    "MCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMA==".to_owned(),
+                ),
+                encoding:    Some(Encoding::Base64),
+                height:      100,
+                id:          9,
+                name:        "L1".to_owned(),
+                offset_x:    None,
+                offset_y:    None,
+                opacity:     1.0,
+                properties:  None,
+                start_x:     None,
+                start_y:     None,
+                visible:     true,
+                width:       100,
+                x:           0,
+                y:           0,
+            })
+            .unwrap(),
+            serde_json::to_string(&TileLayer {
+                chunks:      Some(vec![
+                    Chunk {
+                        data:   Raw(vec![1, 1, 0, 1]),
+                        height: 15,
+                        width:  55,
+                        x:      77,
+                        y:      88,
+                    },
+                    Chunk {
+                        data:   Encoded("MSwgMSwgMCwgMQ==".to_owned()),
+                        height: 6,
+                        width:  66,
+                        x:      0,
+                        y:      0,
+                    },
+                ]),
+                compression: None,
+                data:        Encoded(
+                    "MCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMCwgMA==".to_owned(),
+                ),
+                encoding:    Some(Encoding::Base64),
+                height:      100,
+                id:          9,
+                name:        "L1".to_owned(),
+                offset_x:    None,
+                offset_y:    None,
+                opacity:     1.0,
+                properties:  None,
+                start_x:     None,
+                start_y:     None,
+                visible:     true,
+                width:       100,
+                x:           0,
+                y:           0,
+            })
+            .unwrap(),
+            serde_json::to_string(&TileLayer {
+                chunks:      None,
+                compression: Some(Compression::Zlib),
+                data:        Encoded("eNrzdS5P9yUFO9raAgDo2RIx".to_owned()),
+                encoding:    Some(Encoding::Base64),
+                height:      100,
+                id:          9,
+                name:        "L1".to_owned(),
+                offset_x:    None,
+                offset_y:    None,
+                opacity:     1.0,
+                properties:  None,
+                start_x:     None,
+                start_y:     None,
+                visible:     true,
+                width:       100,
+                x:           0,
+                y:           0,
+            })
+            .unwrap(),
+            serde_json::to_string(&TileLayer {
+                chunks:      None,
+                compression: Some(Compression::Gzip),
+                data:        Encoded("H4sIAAAAAAAA//N1Lk/3JQU72toCAHCxT1Y0AAAA".to_owned()),
+                encoding:    Some(Encoding::Base64),
+                height:      100,
+                id:          9,
+                name:        "L1".to_owned(),
+                offset_x:    None,
+                offset_y:    None,
+                opacity:     1.0,
+                properties:  None,
+                start_x:     None,
+                start_y:     None,
+                visible:     true,
+                width:       100,
+                x:           0,
+                y:           0,
+            })
+            .unwrap(),
+        ];
+
+        for (actual, expected) in actuals.into_iter().zip(expecteds) {
+            assert_eq!(actual, expected);
         }
     }
 }
