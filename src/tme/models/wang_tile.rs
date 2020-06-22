98c24b731dfc381c41b0d12f554688e94a6eb0c6
LazyMechanic <AsharnRus@gmail.com>
2020-06-22 20:38:08 +0300

    Add WangTile tests

--- src/tme/models/wang_tile.rs
+++ src/tme/models/wang_tile.rs
@@ -1,7 +1,7 @@
 use serde::Deserialize;
 use serde::Serialize;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
 #[serde(rename_all = "lowercase")]
 pub struct WangTile {
     #[serde(rename = "dflip")]
@@ -14,4 +14,70 @@ pub struct WangTile {
     pub v_flip:  bool,
     #[serde(rename = "wangid")]
     pub wang_id: Vec<i64>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use serde_json::json;
+
+    #[test]
+    fn deserialize_wang_tile() {
+        let actuals: Vec<WangTile> = serde_json::from_value(json! {
+            [
+                {
+                    "dflip":  true,
+                    "hflip":  false,
+                    "tileid": 555,
+                    "vflip":  true,
+                    "wangid": [56, 789],
+                }
+            ]
+        })
+        .unwrap();
+
+        let expecteds: Vec<WangTile> = vec![WangTile {
+            d_flip:  true,
+            h_flip:  false,
+            tile_id: 555,
+            v_flip:  true,
+            wang_id: vec![56, 789],
+        }];
+
+        for (actual, expected) in actuals.into_iter().zip(expecteds) {
+            assert_eq!(actual, expected);
+        }
+    }
+
+    #[test]
+    fn serialize_wang_tile() {
+        let expecteds: Vec<String> = vec![json! {
+            {
+                "dflip":  true,
+                "hflip":  false,
+                "tileid": 555,
+                "vflip":  true,
+                "wangid": [56, 789],
+            }
+        }]
+        .into_iter()
+        .map(|v| serde_json::to_string(&v).unwrap())
+        .collect();
+
+        let actuals: Vec<String> = vec![WangTile {
+            d_flip:  true,
+            h_flip:  false,
+            tile_id: 555,
+            v_flip:  true,
+            wang_id: vec![56, 789],
+        }]
+        .into_iter()
+        .map(|v| serde_json::to_string(&v).unwrap())
+        .collect();
+
+        for (actual, expected) in actuals.into_iter().zip(expecteds) {
+            assert_eq!(actual, expected);
+        }
+    }
 }
